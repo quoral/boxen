896dee8b5296d4aab23602ab3d91d2962e26641e
Karl Johan Krantz <krantz@spotify.com>
2013-07-13 11:18:37 +0200

    Does this even work!?
    Change-Id: I24319d56e83d41db54c8d14c8849ac1706243795

--- modules/people/manifests/schwomp/dotfiles.pp
+++ modules/people/manifests/schwomp/dotfiles.pp
@@ -6,9 +6,36 @@ class people::schwomp::dotfiles{
       source => "${::github_login}/dotfiles"
     }
 
-    file { "${home}/vim":
+    file { "${home}/.vim":
       ensure  => link,
-      target  => "${dotfiles_dir}/.vim",
+      target  => "${dotfiles_dir}/vim",
+      require => Repository[$dotfiles_dir]
+    }
+
+    file { "${home}/.zshrc":
+      ensure  => link,
+      target  => "${dotfiles_dir}/.zshrc",
+      require => Repository[$dotfiles_dir]
+    }
+
+    file { "${home}/.vimrc":
+      ensure  => link,
+      target  => "${dotfiles_dir}/vimrc",
+      require => Repository[$dotfiles_dir]
+    } 
+    file { "${home}/.emacs.d":
+      ensure  => link,
+      target  => "${dotfiles_dir}/emacs.d",
+      require => Repository[$dotfiles_dir]
+    }
+    file { "${home}/.slate":
+      ensure  => link,
+      target  => "${dotfiles_dir}/slate",
+      require => Repository[$dotfiles_dir]
+    }
+    file { "${home}/.slate.js":
+      ensure  => link,
+      target  => "${dotfiles_dir}/slate.js",
       require => Repository[$dotfiles_dir]
     }
 }
