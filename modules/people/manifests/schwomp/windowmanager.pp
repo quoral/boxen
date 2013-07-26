05fc5165fad08395db869e6118b31a3a5273af32
Karl Johan Krantz <krantz@spotify.com>
2013-07-26 14:39:16 +0200

    remap
    Change-Id: If7304c698725fc05b28ff7cfe7eebd55d339f370

--- modules/people/manifests/schwomp/windowmanager.pp
+++ modules/people/manifests/schwomp/windowmanager.pp
@@ -3,7 +3,7 @@ class people::schwomp::windowmanager{
     
     include slate
     include keyremap4macbook
-    keyremap4macbook::remap{ 'usercustom.f19_to_hyper': }
+    keyremap4macbook::remap{ 'F19_Hyper': }
     keyremap4macbook::set{ 'parameter.keyoverlaidmodifier_timeout':
         value => '200'
     }
